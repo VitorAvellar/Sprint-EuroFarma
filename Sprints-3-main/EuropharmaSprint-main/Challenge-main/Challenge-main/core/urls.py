6f0d34b55a53b8186fc5ed3df5c94b4013d20169
Vitor Avellar <vitor07sa@gmail.com>
2024-09-27 15:58:06 -0300

    Tela de inicio ser o LOGIN

--- Sprints-3-main/EuropharmaSprint-main/Challenge-main/Challenge-main/core/urls.py
+++ Sprints-3-main/EuropharmaSprint-main/Challenge-main/Challenge-main/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', lambda request: redirect('login'), name='home'),
     path('index', views.index, name='index'),
     path('portal_admin', views.admin, name='portal_admin'),
     path('cadastro_geral', views.cadastro_geral, name='cadastro_geral'),
